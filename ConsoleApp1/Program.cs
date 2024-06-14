1a22c7c5a2e4ea8e9805030941bc076dcc1d77e6
Vinicius <viniciusferreiramello@gmail.com>
2024-06-14 20:38:59 -0300

    Versão 2.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,19 +1,16 @@
-﻿namespace ConsoleApp1
+﻿using System.Linq;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
-        static Espaco[] espacos = {
-            new Espaco('A', 100, 10000),
-            new Espaco('B', 100,10000),
-            new Espaco('C', 100,10000),
-            new Espaco('D', 100,10000),
-            new Espaco('E', 200,17000),
-            new Espaco('F', 200,17000),
-            new Espaco('G', 50,8000),
-            new Espaco('H', 500,35000)
-            };
+        static Empresa empresa = new Empresa();
+        static Evento evento;
+        static Espaco espaco;
         static void Main(string[] args)
         {
+
             Console.WriteLine("Bem-vindo ao AGGV Festas\n");
             Console.WriteLine("Qual tipo de evento você deseja contratar?\n");
             Console.WriteLine("1 - Casamento\n2 - Formatura\n3 - Festa de empresa\n4 - Festa de aniversário\n5 - Evento livre\n");
@@ -24,7 +21,7 @@
                 case 1:
                     contratarCasamento();
                     break;
-                case 2:
+                /*case 2:
                     contratarFormatura();
                     break;
                 case 3:
@@ -35,26 +32,32 @@
                     break;
                 case 5:
                     contratarEventoLivre();
-                    break;
+                    break;*/
                 default:
                     Console.WriteLine("Opção inválida, digite apenas uma opção existente");
                     break;
             }
         }
-        static Espaco menuEscolhaEspaco()
+        static Espaco menuEscolhaEspaco(DateTime data)
         {
             Console.WriteLine("Perfeito! Chegou a hora de apresentar nossas opções de espaço que melhor se adeque à cerimônia desejada");
-            foreach (var espaco in espacos)
+            foreach (var espaco in empresa._listaEspacos)
             {
                 Console.WriteLine($"Espaço {espaco.nomeEspaco} - Capacidade: {espaco.capacidadeMaxima} pessoas: R$ {espaco.valorEspaco}");
             }
             Console.WriteLine("\nDigite o nome do espaço que te atende melhor:");
-            char nomeEspaco = char.Parse(Console.ReadLine());
-            for (int i = 0; i < espacos.Length; i++)
+            string nomeEspaco = Console.ReadLine();
+            for (int i = 0; i < empresa._listaEspacos.Count; i++)
             {
-                if (nomeEspaco == espacos[i].nomeEspaco)
+                if (empresa._listaEspacos[i].nomeEspaco.Equals(nomeEspaco))
                 {
-                    return espacos[i];
+                    bool ehPossivelRealizar = empresa.verificaEspacoDisponivel(empresa._listaEspacos[i], data);
+                    if (ehPossivelRealizar)
+
+                        return empresa._listaEspacos[i];
+
+                    else
+                        return null;
                 }
             }
             return null;
@@ -65,7 +68,7 @@
             int qtdConvidados;
             while (true)
             {
-                Console.WriteLine("Quantas convidados terão no casamento?");
+                Console.WriteLine("Quantos convidados terão no casamento?");
                 if (int.TryParse(Console.ReadLine(), out qtdConvidados))
                 {
                     if (qtdConvidados < 1)
@@ -86,20 +89,22 @@
                     Console.WriteLine("Por favor, insira um número válido.");
                 }
             }
-
-            Espaco espacoDesejado = menuEscolhaEspaco();
-            if (espacoDesejado != null)
+            Console.WriteLine("Certo, agora precisamos saber a data que você deseja realizar o casamento. Segue abaixo algumas de nossas regras:\n\n");
+            Console.WriteLine("- Os casamentos são realizados somente às sextas e sábados.\n- Agendamentos de datas apenas com 30 dias de antecedência\n- Realizamos apenas um casamento por dia");
+            Console.WriteLine("\n\nDigite uma data no formato DD/MM/AAAA para ser realizado do casamento:");
+            string data = Console.ReadLine();
+            DateTime dataCasamento;
+            if (DateTime.TryParse(dataCasamento, out data))
             {
-                Console.WriteLine("Certo, agora precisamos saber a data que você deseja realizar o casamento. Segue abaixo algumas de nossas regras:\n\n");
-                Console.WriteLine("- Os casamentos são realizados somente às sextas e sábados.\n- Agendamentos de datas apenas com 30 dias de antecedência\n- Realizamos apenas um casamento por dia");
-                Console.ReadLine();
+                
             }
             else
             {
-                Console.WriteLine("Espaço não encontrado. Tente novamente.");
+                Console.WriteLine("A data informada é inválida");
             }
+            Espaco espacoDesejado = menuEscolhaEspaco(dataCasamento);
         }
-        static void contratarFormatura()
+        /*static void contratarFormatura()
         {
             menuEscolhaEspaco();
         }
@@ -114,7 +119,7 @@
         static void contratarEventoLivre()
         {
             menuEscolhaEspaco();
-        }
+        }*/
 
     }
 }
--- ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Release/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63052a92ad1fdd1465c010e224018b19464bd8db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+318cb6307cc58230ce3ec402515c7c7fb34ecfd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
